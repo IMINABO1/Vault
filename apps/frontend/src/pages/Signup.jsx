21059acdfe30cc17eeb28074b7e293ed01ffdaf9
StephienOledibe <168848922+StephienOledibe@users.noreply.github.com>
2026-02-07 02:13:35 +0000

    Added layout, sidebar, and pages

---
+++ apps/frontend/src/pages/Signup.jsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <div className="p-10 text-2xl">
+      Signup Page (Coming Soon)
+    </div>
+  );
+}
---
+++ apps/frontend/src/pages/Verification.jsx
@@ -0,0 +1,7 @@
+export default function Verification() {
+  return (
+    <div className="p-10 text-2xl">
+      Verification Page (Coming Soon)
+    </div>
+  );
+}
